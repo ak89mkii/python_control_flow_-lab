50eb9f477a1a8c600c10700ffc64077ecaa0ccd2
A.I. Kasem <ak89mkii@gmail.com>
2020-09-28 16:23:14 -0400

    first commit

---
+++ python_control_flow_lab.py
@@ -0,0 +1,49 @@
+# PROBLEM 01:
+
+# cvcheck = input('Please enter a letter from the alphabet:')
+
+# if cvcheck in ('a', 'e', 'i', 'o', 'u') and cvcheck.isalpha() == True and len(cvcheck) <= 1:
+#     print(f'The letter {cvcheck} is a vowel')
+
+# elif cvcheck.isalpha() == True and len(cvcheck) <= 1:
+#     print(f'The letter {cvcheck} is a consonant')
+
+# else:
+#     print(f'The letter {cvcheck} is a neither')
+
+
+# PROBLEM 2:
+
+# phrase = input('Please enter a word or phrase:')
+# result = len(phrase)
+
+# if phrase != "quit":
+#     print(f'What you entered is {result} characters long.')
+    
+# else:
+#     print("exit")
+
+
+# PROBLEM 3:
+
+# humanyear = int(input("Input a dog's age in human years:"))
+# dogage = humanyear * 7 + 6
+
+# if humanyear == 1:
+#     print("The dog's age in dog years is 10")
+
+# elif humanyear == 2:
+#     print("The dog's age in dog years is 20")
+
+# else:
+#     print(f"The dog's age in dog years is {dogage}")
+
+
+# PROBLEM 4:
+
+
+
+# PROBLEM 5:
+
+# PROBLEM 6:
+
